16bb8c805f6e1bed83784e41bd287a5948c161dc
Victor Joseph <9651630+victorvasujoseph@users.noreply.github.com>
2019-01-22 21:23:45 -0800

    Code Clean up

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,84 +1,31 @@
 var friends = [
-    {
-        "name": "Racheal",
-        "photo": "",
-        "scores": [
-            5,
-            1,
-            4,
-            4,
-            5,
-            1,
-            2,
-            5,
-            4,
-            1
-        ]
-    },
-    {
-        "name": "Chandler",
-        "photo": "",
-        "scores": [
-            2,
-            4,
-            5,
-            5,
-            2,
-            1,
-            4,
-            3,
-            4,
-            5
-        ]
-    },
-    {
-        "name": "Ross",
-        "photo": "",
-        "scores": [
-            1,
-            2,
-            1,
-            4,
-            5,
-            2,
-            3,
-            2,
-            5,
-            3
-        ]
-    },
-    {
-        "name": "Joey",
-        "photo": "",
-        "scores": [
-            3,
-            2,
-            1,
-            5,
-            4,
-            3,
-            4,
-            5,
-            2,
-            1,
-        ]
-    },
-    {
-        "name": "Phebe",
-        "photo": "",
-        "scores": [
-            4,
-            1,
-            5,
-            1,
-            2,
-            4,
-            5,
-            1,
-            5,
-            3,
-        ]
-    }
+  {
+    name: "Racheal",
+    photo:
+      "https://pbs.twimg.com/profile_images/691785039043022849/oWsy8LNR.jpg",
+    scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+  },
+  {
+    name: "Chandler",
+    photo:
+      "https://pbs.twimg.com/profile_images/691785039043022849/oWsy8LNR.jpg",
+    scores: [2, 4, 5, 5, 2, 1, 4, 3, 4, 5]
+  },
+  {
+    name: "Ross",
+    photo: "https://avatars2.githubusercontent.com/u/8504998?v=3&s=460",
+    scores: [1, 2, 1, 4, 5, 2, 3, 2, 5, 3]
+  },
+  {
+    name: "Joey",
+      photo: "https://avatars2.githubusercontent.com/u/8504998?v=3&s=460",
+    scores: [3, 2, 1, 5, 4, 3, 4, 5, 2, 1]
+  },
+  {
+    name: "Phebe",
+      photo: "https://avatars2.githubusercontent.com/u/8504998?v=3&s=460",
+    scores: [4, 1, 5, 1, 2, 4, 5, 1, 5, 3]
+  }
 ];
 
 module.exports = friends;
